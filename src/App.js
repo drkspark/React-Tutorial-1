06ad9b83617c40691b1275c9844cdd99061fbfa7
drkspark <madhusudan753@gmail.com>
2023-03-15 14:57:24 +0530

    Done with 3rd tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // This is returing JSX
+
+    // We can freely print out String and Number
+    const title = "Welcome to the new Blog";
+    const likes = 50;
+
+    const link = "https://www.google.com/";
+
+    // Objects can't be printrd out
+    // const person = {name: 'Madhu', age: 21};
+
+    return (
+        <div className="App">
+            <div className="content">
+                <h1>{title}</h1>
+                <p>Liked {likes} times</p>
+                {/* <p>Here's the object {person}</p> */}
+
+                {/* We can directly output Dynamice values onto screen */}
+                <p>{"Hello Pandu's"}</p>
+                <p>{[1, 2, 3, 4, 5]}</p>
+                <p>{Math.random() * 100}</p>
+
+                {/* The value of the link is dynamic */}
+                <a href={link}>Open Site</a>
+            </div>
+        </div>
+    );
 }
 
 export default App;
